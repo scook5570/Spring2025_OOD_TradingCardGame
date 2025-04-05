224d31f9ade6dd3a29f0da20c0e3b004caaed312
scook5570 <cooksm@merrimack.edu>
2025-04-04 21:00:42 -0400

    Created basic card object

---
+++ Card.java
@@ -0,0 +1,41 @@
+import java.io.File;
+
+class Card {
+    private String name; // Name of the card
+    private int rarity; // Rarity of the card (1-5)
+    private File image; // Image of the card
+    
+    /**
+     * Trading card constructor
+     * @param name name of the card
+     */
+    Card(String name, int rarity, File image) {
+        this.name = name;
+        this.rarity = rarity;
+        this.image = image;
+    }
+
+    /**
+     * Get the name of the card
+     * @return name of the card
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Get the rarity of the card
+     * @return rarity of the card
+     */
+    public int getRarity() {
+        return rarity;
+    }
+
+    /**
+     * Get the image of the card
+     * @return image of the card
+     */
+    public File getImage() {
+        return image;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TradingCardGame
+
+This is the final project for Object Oriented Design.
+In this project we set out to create a basic trading card game where users can pull random trading cards and trade them with others.
