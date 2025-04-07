01c630c13c83b3d5127d5d3c4bb11ff4655bfeec
scook5570 <cooksm@merrimack.edu>
2025-04-07 19:14:59 -0400

    Example of server communication, committing for self ref

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,7 +1,36 @@
 package client;
 
+import java.net.Socket;
+
+import shared.MessageSocket;
+import shared.messages.Message;
+import shared.messages.UserCredRequest;
+
 public class Client {
     public static void main(String[] args) {
+        String serString = "localhost";
+        int port = 5000;
 
+        try {
+            Socket socket = new Socket(serString, port);
+            System.out.println("Connected to server at " + serString + ":" + port);
+
+            MessageSocket messageSocket = new MessageSocket(socket);
+            System.out.println("MessageSocket created");
+
+            UserCredRequest userCredRequest = new UserCredRequest("Login", "username", "password");
+            System.out.println("Sending UserCredRequest: " + userCredRequest.toJSONType().toString());
+
+            messageSocket.sendMessage(userCredRequest);
+            System.out.println("UserCredRequest sent");
+
+            Message response = messageSocket.getMessage();
+
+            messageSocket.close();
+            socket.close();
+            System.out.println("Socket closed");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
