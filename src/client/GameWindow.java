c4f58e82e743d14248926a97843e655f5f5b33e1
nayeliMC26 <villan@merrimack.edu>
2025-04-12 02:37:55 -0400

    Feat: Created a very basic home screen gui setup w/placeholders temp hardcoding

--- src/client/GameWindow.java
+++ src/client/GameWindow.java
@@ -3,8 +3,6 @@ package client;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -12,7 +10,9 @@ import java.io.IOException;
 public class GameWindow extends JFrame {
     private JPanel userInfoPanel;
     private JButton tradeButton;
-    private JLabel statusLabel;
+    private JButton galleryButton;
+    private JButton homeButton;
+    private JPanel bottomPanel;
 
     public GameWindow() throws IOException {
         setTitle("Home");
@@ -21,24 +21,95 @@ public class GameWindow extends JFrame {
         setSize(screenSize.width, screenSize.height);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(null);
+        setLayout(new BorderLayout());
 
         int panelWidth = 375;
         int panelHeight = 50;
-
         int panelX = screenSize.width - panelWidth;
         int panelY = 0;
 
         userInfoPanel = new JPanel(new BorderLayout());
         userInfoPanel.setBounds(panelX, panelY, panelWidth, panelHeight);
         userInfoPanel.setBackground(new Color(217, 217, 217));
+        userInfoPanel.setLayout(new BoxLayout(userInfoPanel, BoxLayout.X_AXIS));
         add(userInfoPanel);
 
 
         BufferedImage myPicture = ImageIO.read(new File("assets/user.png"));
         Image scaledImage = myPicture.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
         JLabel picLabel = new JLabel(new ImageIcon(scaledImage));
-        userInfoPanel.add(picLabel, BorderLayout.WEST);
+
+        // temporary until we can obtain user from database
+        JLabel usernameLabel = new JLabel("username");
+        usernameLabel.setFont(new Font("Roboto", Font.PLAIN, 16));
+
+        picLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        usernameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        userInfoPanel.add(picLabel);
+        userInfoPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+        userInfoPanel.add(usernameLabel);
+
+        // Card carousel
+        JPanel carouselPanel = new JPanel();
+        carouselPanel.setLayout(new BoxLayout(carouselPanel, BoxLayout.X_AXIS));
+        carouselPanel.setBackground(Color.WHITE);
+
+        // 3.5 x 2.5 ratio
+        Dimension centerPackSize = new Dimension(250, 375);
+        Dimension sidePackSize = new Dimension(210, 315);
+
+        carouselPanel.add(wrapWithContainer(createPackPanel(sidePackSize), sidePackSize));
+        carouselPanel.add(Box.createRigidArea(new Dimension(75, 0)));
+        carouselPanel.add(wrapWithContainer(createPackPanel(centerPackSize), centerPackSize));
+        carouselPanel.add(Box.createRigidArea(new Dimension(75, 0)));
+        carouselPanel.add(wrapWithContainer(createPackPanel(sidePackSize), sidePackSize));
+
+        JPanel centerWrapper = new JPanel(new GridBagLayout());
+        centerWrapper.setBackground(Color.WHITE);
+        centerWrapper.add(carouselPanel);
+        add(centerWrapper, BorderLayout.CENTER);
+
+        bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 10));
+        bottomPanel.setBackground(new Color(200, 200, 200));
+
+        galleryButton = new JButton("Gallery");
+        galleryButton.setPreferredSize(new Dimension(100, 40));
+
+        homeButton = new JButton("Home");
+        homeButton.setPreferredSize(new Dimension(100, 40));
+
+        tradeButton = new JButton("Trade");
+        tradeButton.setPreferredSize(new Dimension(100, 40));
+
+        bottomPanel.add(galleryButton);
+        bottomPanel.add(homeButton);
+        bottomPanel.add(tradeButton);
+
+        add(bottomPanel, BorderLayout.SOUTH);
+    }
+
+    /**
+     * Method to make the pack panel
+     * @param size
+     * @return
+     */
+    private JPanel createPackPanel(Dimension size) {
+        JPanel pack = new JPanel();
+        pack.setPreferredSize(size);
+        pack.setBackground(new Color(100, 100, 100));
+        pack.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        return pack;
+    }
+
+    private JPanel wrapWithContainer(JPanel content, Dimension size) {
+        JPanel container = new JPanel();
+        container.setOpaque(false);
+        container.setPreferredSize(size);
+        container.setMaximumSize(size);
+        container.setMinimumSize(size);
+        container.setLayout(new GridBagLayout());
+        container.add(content);
+        return container;
     }
 }
-
