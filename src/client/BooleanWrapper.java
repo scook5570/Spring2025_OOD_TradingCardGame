8e627cac448ae620678b77710ba37d10dcea935b
Mike Brown <brownbk@merrimack.edu>
2025-04-13 11:53:03 -0400

    bolean wrapper class
    just a wrapper class to work around the requirement of final variables in the lambda expressions

---
+++ src/client/BooleanWrapper.java
@@ -0,0 +1,9 @@
+package client;
+
+/**
+ * Helper class for Client.java
+ * Work-around for the final variable type requirement in the lambda expressions
+ */
+public class BooleanWrapper {
+    boolean value;
+}
